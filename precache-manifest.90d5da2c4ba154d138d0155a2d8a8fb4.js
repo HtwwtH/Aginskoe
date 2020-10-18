7d2c75a5f9cec19f3a8eae1c17bb1435f7dd1252
HtwwtH <alina.l22@mail.ru>
2020-10-19 00:08:57 +0700

    Updates

---
+++ precache-manifest.90d5da2c4ba154d138d0155a2d8a8fb4.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a219ef75140d19926e74b2ccae83936",
+    "url": "/Aginskoe/index.html"
+  },
+  {
+    "revision": "86f6454864479d9c14c8",
+    "url": "/Aginskoe/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "b3e0946fc03a9410889c",
+    "url": "/Aginskoe/static/css/main.21eebb0a.chunk.css"
+  },
+  {
+    "revision": "86f6454864479d9c14c8",
+    "url": "/Aginskoe/static/js/2.a53c189a.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/Aginskoe/static/js/2.a53c189a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3e0946fc03a9410889c",
+    "url": "/Aginskoe/static/js/main.597bafb6.chunk.js"
+  },
+  {
+    "revision": "b287b656a665a9c6a5f5",
+    "url": "/Aginskoe/static/js/runtime-main.ed6cbf19.js"
+  },
+  {
+    "revision": "edb7b517a52bdef06e4d1e1842aff2d2",
+    "url": "/Aginskoe/static/media/1.edb7b517.jpg"
+  },
+  {
+    "revision": "f127af7006bf3132fe64905f5650074f",
+    "url": "/Aginskoe/static/media/2.f127af70.jpg"
+  },
+  {
+    "revision": "27168d16a632b7fa5edac838261e553e",
+    "url": "/Aginskoe/static/media/3.27168d16.jpg"
+  },
+  {
+    "revision": "4b010011e8ab88ea87f08f0d5812e485",
+    "url": "/Aginskoe/static/media/4.4b010011.jpg"
+  },
+  {
+    "revision": "b8ddb104791d086f35eb7c7c9444caae",
+    "url": "/Aginskoe/static/media/5.b8ddb104.jpg"
+  },
+  {
+    "revision": "f931a9b93c77d0289137bd0e4eb76501",
+    "url": "/Aginskoe/static/media/card1.f931a9b9.jpg"
+  },
+  {
+    "revision": "b568996a1b1753f159e4135098387a61",
+    "url": "/Aginskoe/static/media/card2.b568996a.jpg"
+  },
+  {
+    "revision": "dde6611adf7f623a5f00ec2f683fa5c7",
+    "url": "/Aginskoe/static/media/card3.dde6611a.jpg"
+  },
+  {
+    "revision": "93b3d6e3488c01333e69e76dcbd41435",
+    "url": "/Aginskoe/static/media/history.93b3d6e3.jpg"
+  },
+  {
+    "revision": "4ce636f1317d88cc7b072aa5c480be69",
+    "url": "/Aginskoe/static/media/img1.4ce636f1.jpg"
+  },
+  {
+    "revision": "406fcfa22342438496cc7d29411f6728",
+    "url": "/Aginskoe/static/media/img2.406fcfa2.jpg"
+  },
+  {
+    "revision": "c24f6880d3d13916f9b9e7e019ac6078",
+    "url": "/Aginskoe/static/media/img3.c24f6880.jpg"
+  }
+]);
